4e3758d1ce0a6b54af0e0a8599969470b2a3d7d4
yuchien.shih <yuchien.shih@flowring.com>
2022-05-23 15:46:58 +0800

    初始化專案

---
+++ src/components/login/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>hi</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import login from "@/components/login/login.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    login,
+  },
+};
 </script>
